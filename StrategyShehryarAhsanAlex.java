563ab003e9b795c643fe239190158aca2139af21
asanaullah2015 <asanaullah2015@fau.edu>
2020-03-24 22:34:13 -0400

    Added our Strategy

--- StrategyShehryarAhsanAlex.java
+++ StrategyShehryarAhsanAlex.java
@@ -3,16 +3,31 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 public class StrategyShehryarAhsanAlex extends Strategy {
+	public Queue<Integer> history;
+	public int historyLength;
+	public double historySum;
         public StrategyShehryarAhsanAlex() {
+	    history = new LinkedList<>();
+	    historyLength = 4;
+	    history.add(0);
+	    history.add(0);
+	    history.add(1);
+	    history.add(1);
+	    historySum = 2;
             name = "Shehryar, Ahsan, Alex";
         }
 
         public int nextMove() {
-            if (Math.random() < 0.5) {
-                return 1;
-            }
-            else {
+            if (historySum/(double)historyLength<0.5){
                 return 0;
             }
+            return 1;
         }
+
+	public void saveOpponentMove(int move) {
+		historySum -= history.remove();
+		history.add(move);
+		historySum += move;
+		opponentLastMove = move;
+	}
 }
